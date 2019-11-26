226561ec3001b06d1a21a845ef0111777e19d7ae
zhenze zhao <c00198551@itcarlow.ie>
2019-11-26 16:56:12 +0000

    adding new component

---
+++ P2_MainGame_TeamB/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("do something");
+        if (collision.tag == "balloon")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
