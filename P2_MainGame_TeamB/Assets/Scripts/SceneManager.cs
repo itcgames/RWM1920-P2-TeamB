f1d798f3a929fc9b846514050288e7f53911102e
patryk wysocki <c00213855@itcarlow.ie>
2019-11-27 10:53:46 +0000

    creating prefabs and spawning them in the middle by click of a button

---
+++ P2_MainGame_TeamB/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject bomb;
+    public Button myButton;
+
+    private bool m_isHeldDown;
+    private float m_startPosX;
+    private float m_startPosY;
+    Vector3 m_mousePos;
+    void OnEnable()
+    {
+
+        myButton.onClick.AddListener(TaskOnClick);//adds a listener for when you click the button
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left button.");
+            m_mousePos = Input.mousePosition;
+            m_mousePos = Camera.main.ScreenToWorldPoint(m_mousePos);
+
+            m_startPosX = m_mousePos.x - transform.localPosition.x;
+            m_startPosY = m_mousePos.y - transform.localPosition.y;
+
+            m_isHeldDown = true;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        m_isHeldDown = false;
+    }
+    public bool getObjectHeldDown()
+    {
+        return m_isHeldDown;
+    }
+    public void SetHeldDown(bool t_heldDown)
+    {
+        m_isHeldDown = t_heldDown;
+    }
+    public void AddObject()
+    {
+        Instantiate(bomb);
+        m_isHeldDown = true;
+
+    }
+
+    void TaskOnClick()
+    {
+        AddObject();
+        Debug.Log("You have clicked the button!");
+    }
+}
