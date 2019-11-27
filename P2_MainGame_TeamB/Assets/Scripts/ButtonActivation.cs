c24b0b1bedd03b6d3d44ecf4cffda2d3920959b1
Eoin <c002217717@itcarlow.ie>
2019-11-27 13:21:48 +0000

    Pre Merge

---
+++ P2_MainGame_TeamB/Assets/Scripts/ButtonActivation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivation : MonoBehaviour
+{
+    public GameObject toBeActivated;
+
+    private void Start()
+    {
+        toBeActivated.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D t_other)
+    {
+        if (t_other.CompareTag("ball"))
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+}
--- P2_MainGame_TeamB/Assets/Scripts/CameraFollow.cs
+++ P2_MainGame_TeamB/Assets/Scripts/CameraFollow.cs
@@ -6,17 +6,18 @@ public class CameraFollow: MonoBehaviour
 {
 
     public GameObject ball;
-    private Vector3 camOffset;
+    private float camOffsetZ;
 
     // Start is called before the first frame update
     void Start()
     {
-        camOffset = transform.position - ball.transform.position;
+        camOffsetZ  = transform.position.z - ball.transform.position.z;
+        transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z + camOffsetZ);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = ball.transform.position + camOffset;
+        transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z + camOffsetZ);
     }
 }
