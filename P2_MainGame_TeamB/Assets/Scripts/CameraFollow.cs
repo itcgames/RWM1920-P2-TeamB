72d0193f045892c6100fa73d35c02f3706e4ae31
Eoin <c002217717@itcarlow.ie>
2019-11-27 10:33:44 +0000

    camera following

---
+++ P2_MainGame_TeamB/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow: MonoBehaviour
+{
+
+    public GameObject ball;
+    private Vector3 camOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camOffset = transform.position - ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = ball.transform.position + camOffset;
+    }
+}
