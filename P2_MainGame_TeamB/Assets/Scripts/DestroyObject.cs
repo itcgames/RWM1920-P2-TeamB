f8a6e9de7e710e47b21d53c0dffe260396ae1f1d
Eoin <c002217717@itcarlow.ie>
2019-11-28 12:22:25 +0000

    Eoins Level WIP

--- P2_MainGame_TeamB/Assets/Scripts/DestroyObject.cs
+++ P2_MainGame_TeamB/Assets/Scripts/DestroyObject.cs
@@ -2,15 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DestroyObject : MonoBehaviour
 {
+    private SceneManager manager;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         Debug.Log("do something");
         if (collision.tag == "balloon")
         {
             Destroy(collision.gameObject);
         }
+        
     }
 }
