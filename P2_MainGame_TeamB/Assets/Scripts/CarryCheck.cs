a5d9348049732e48ed1cd5197c0c8961d2266598
patryk wysocki <c00213855@itcarlow.ie>
2019-11-29 16:06:27 +0000

    Build and Final Commit

--- P2_MainGame_TeamB/Assets/Scripts/CarryCheck.cs
+++ P2_MainGame_TeamB/Assets/Scripts/CarryCheck.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class CarryCheck : MonoBehaviour
 {
+    public Text hint;
+
     private bool onload = false;
     private bool release = false;
     private string goods = " ";
     private GameObject player;
 
+    private void Start()
+    {
+        hint.text = " ";
+    }
+
     private void Update()
     {
+        if (onload && goods == "Player")
+        {
+            hint.text = "Press 'Space' To Release";
+        }
 
         if (Input.GetKeyDown("space") && onload && goods == "Player")
         {
             Debug.Log(goods);
             release = true;
+            hint.text = " ";
             Destroy(player.GetComponent<HingeJoint2D>());
         }
     }
@@ -29,6 +42,8 @@ public class CarryCheck : MonoBehaviour
             collision.gameObject.AddComponent<HingeJoint2D>();
             collision.gameObject.GetComponent<HingeJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
             onload = true;
+
+
         }    
     }
 
