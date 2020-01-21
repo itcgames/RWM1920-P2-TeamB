b3389caea487bd61c27cac2bf34c135c319a8adc
Eoin <c002217717@itcarlow.ie>
2020-01-21 16:24:44 +0000

    added highscore Table, tested on Phone

---
+++ P2_MainGame_TeamB/Assets/Scripts/HighScoreTable/HighScoreEntryData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace P3.HighScores
+{
+    [Serializable]
+    public struct HighScoreEntryData 
+    {
+        public string m_name;
+        public float m_time;
+    }
+}
