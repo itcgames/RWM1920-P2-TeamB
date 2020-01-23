f4f7acb0319c4854849c2aaeeba50707e5d2fde3
Eoin <c002217717@itcarlow.ie>
2020-01-23 16:08:54 +0000

    Started testing

---
+++ P2_MainGame_TeamB/Assets/Tests/EoinTests/HighScoreTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    [TestFixture]
+        public class HighScoreTest
+    {
+
+        GameObject Table;
+        [SetUp]
+        public void Setup()
+        {
+            Table = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/HighScore_Table/HighScoreTable"));
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(Table);    
+        }
+
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void HighScoreTestSimplePasses()
+        {
+
+            Table.GetComponent<HighScoreScript>().addEntry();
+
+        }
+
+    }
+}
