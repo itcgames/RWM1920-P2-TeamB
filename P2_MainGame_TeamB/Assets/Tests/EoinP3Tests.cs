9e9cdaa39e6e86b6816cc9f47dd355b0a225efa9
Eoin <c002217717@itcarlow.ie>
2020-01-24 15:12:38 +0000

    tried to get tests working

---
+++ P2_MainGame_TeamB/Assets/Tests/EoinP3Tests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using System.IO;
+
+namespace Tests
+{
+    [TestFixture]
+    public class EoinP3Tests
+    {
+
+        private string SavePath => $"{Application.persistentDataPath}/highscoresTest.json";
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [UnityTest]
+        public IEnumerator ReadingWritingJSonTest()
+        {
+            string json = JsonUtility.ToJson("WritingTest", true);
+            File.WriteAllText(SavePath, json);
+
+            string newJson = JsonUtility.FromJson<string>(json);
+
+            yield return null;
+        }
+
+    }
+}
