6d74ee111355dbf7bebc258fe28845d85623306d
Dion Buckley <dion-b@hotmail.com>
2020-01-24 11:07:29 +0000

    Quick Fix allowing project testing - deleted all submodule Script Assemblies and Test folders

---
+++ P2_MainGame_TeamB/Assets/Scripts/BalloonMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonMovement : MonoBehaviour {
+
+    public float force;
+    public Vector2 balloonPosition;
+
+    private Rigidbody2D rb2d;
+    private float moveVertical = 0;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    public static float getVelocity(float force, float mass)
+    {
+        float acceleration = (force - mass) / mass; // force = mass * acceleration
+
+        float velocity = acceleration * Time.deltaTime; // velocity = acceleration * time
+        
+        return velocity;
+    }
+
+    void FixedUpdate()
+    {
+        moveVertical = getVelocity(force, rb2d.mass);
+
+        Debug.Log(moveVertical);
+        Vector2 movement = new Vector2(0, moveVertical);
+
+        rb2d.AddForce(movement);
+
+        balloonPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ P2_MainGame_TeamB/Assets/Scripts/RotateFanBlade.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateFanBlade : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    public GameObject blade;
+
+
+    void Update()
+    {
+        blade.transform.Rotate(rotationSpeed, 0, 0);
+    }
+}
